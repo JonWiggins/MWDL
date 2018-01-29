3408ef62d5bb1d864c89907177f844d18a371f4c
Jon Wiggins <me@jonwiggins.org>
2018-01-29 13:40:41 -0700

    Somebody touca ma spaghet

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,9 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
- * Just use this to test methods in a closed enviroment
+ * Just use this to test methods in a closed environment
+ *
+ * I frequently used this class to run DataFetcher methods, ie, how many in/active collections are there?
  *
  * @author Jonathan Wiggins
  * @version 6/28/17
@@ -16,15 +18,10 @@ import java.util.ArrayList;
 public class Test {
     public static void main(String[] args) {
         try {
-
-
-            for(String c : DataFetcher.getInactiveCollectionLines())
-                System.out.println(c);
-
+            System.out.println(DataFetcher.getInactiveCollectionLines().size());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
 
     }
 }
--- src/org/mwdl/scripts/analytics/AnalyticsEmailGenerator.java
+++ src/org/mwdl/scripts/analytics/AnalyticsEmailGenerator.java
@@ -3,12 +3,10 @@ package org.mwdl.scripts.analytics;
 /**
  * Haha, JK. You need the C# script for this, nice bamboozle tho, eh?
  *
+ * If you are reading this, and don't have that script, email me 'at' jonwiggins.org asking for the VS projects
+ * for MWDL emails and I'll send it to you.
+ *
  * @author Jonathan Wiggins
  * @version 11/10/17
  */
-public class AnalyticsEmailGenerator {
-
-    public static void main(String[] args) {
-
-    }
-}
+public class AnalyticsEmailGenerator {}
