18742a2508ce360b55b4eae6bdea99497561a2fe
Jon Wiggins <me@jonwiggins.org>
2018-06-06 12:22:14 -0600

    Add First Interview Question

---
+++ src/org/mwdl/interview/MergeLists.java
@@ -0,0 +1,12 @@
+package org.mwdl.interview;
+
+/**
+ * TODO - Fill in class desc
+ *
+ * @author Jonathan Wiggins
+ * @version 6/6/18
+ */
+public class MergeLists {
+
+
+}
---
+++ src/org/mwdl/interview/ReverseLinkedList.java
@@ -0,0 +1,87 @@
+package org.mwdl.interview;
+
+public class ReverseLinkedList {
+
+    /**
+     * Given Code.
+     * Note that this is a static class
+     *
+     * Note the use of non-private fields.
+     *  - Using Getters and Setters is not needed
+     *
+     */
+    public static class Node{
+        Node next;
+        Object data;
+
+        public Node(Object data){
+            this.data = data;
+        }
+    }
+
+    /**
+     * Task: Create a method that reverses the ordering of a linked list
+     * - A public method
+     * - Returns a Node
+     * - Takes a Node, the head of a linked list
+     * - The Node that is returned is the new head of the list
+     *  - Previously the tail
+     */
+
+
+    /**
+     * Possible solution
+     * Note this is Omega(n)
+     *
+     * @param head the head of a linked list
+     * @return The new head of the list
+     */
+    public static Node reverse(Node head){
+        Node current = null;
+        Node last;
+        Node next;
+        next = head;
+
+        while(next != null){
+            last = current;
+            current = next;
+            next = next.next;
+            current.next = last;
+        }
+        return current;
+    }
+
+    /**
+     * Test
+     */
+    public static void main(String[] args) {
+        int[] toTest = new int[]{2,3,4,5,6,7,8,9,10};
+        Node head = new Node(1);
+        Node current = head;
+
+        for(int element : toTest){
+            Node toAdd = new Node(element);
+
+            current.next = toAdd;
+            current = toAdd;
+        }
+
+
+        System.out.println("Original");
+        Node currentP = head;
+        while (currentP != null){
+            System.out.println(currentP.data);
+            currentP = currentP.next;
+        }
+
+
+        System.out.println("Reversed");
+        Node newHead = reverse(head);
+        Node newCurrent = newHead;
+        while(newCurrent != null){
+            System.out.println(newCurrent.data);
+            newCurrent = newCurrent.next;
+        }
+    }
+
+}
